9ddd0d73f797bce3b9cce4210c6edf6950d2cccf
salwajid81 <salwajid81@gmail.com>
2024-08-12 12:06:02 -0400

    finished Project
    completed script section of project

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 // Add your code to this file
+let body = document.querySelector("body");
+body.style.backgroundColor ="darkblue";
+
+let title =document.querySelector("h1");
+title.style.textDecoration = "underline";
+
+let title2 =document.querySelector("h2");
+title2.style.textAlign = "center";
+title2.innerHTML = "Here are Pokemons's incredible Gen 1 starters!"
+
+let pokemons = document.querySelector("img")
+pokemons.style.border = "5px dotted orange";
+
+let containers = document.getElementsByClassName("pokemon_images");
+for (let i=0; i<containers.length; i++){
+    containers[i].style.border ="5px dotted orange"
+}
+
+let footer = document.querySelector("footer");
+footer.style.textDecoration = "underline";
+footer.innerHTML ="All Star Code - DOM Project";
+footer.style.fontFamily='Times-New-Roman'
+
+let names = document.querySelectorAll("h3");
+    for (let i = 0; i < names.length; i++) {
+        names[i].style.color = "#FDEFA6";
+        names[i].innerHTML += ('!');
+    }
